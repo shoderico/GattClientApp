1995c990d479f5acc7d00f1ef2696f6161b23814
shoderico <yanagida@shoderitz.com>
2025-05-05 13:40:02 +0900

    Initial commit: Just the .NET MAUI App project created by the VS2022

---
+++ GattClientApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GattClientApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GattClientApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GattClientApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
